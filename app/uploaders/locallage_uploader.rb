75bfe70afdffb3ce97bd0ea41842f372b3e8e5db
EC2 Default User <ec2-user@ip-172-31-39-27.us-east-2.compute.internal>
2019-03-01 15:26:19 +0000

    fix resize with minimagick gem through install imagemagick

--- app/uploaders/locallage_uploader.rb
+++ app/uploaders/locallage_uploader.rb
@@ -1,17 +1,17 @@
 class LocallageUploader < CarrierWave::Uploader::Base
-  
   include CarrierWave::MiniMagick
   
   storage :fog
   
-  # version :medium_avatar do
-  #   # returns a 50x50 image
-  #   process :resize_to_fill => [50, 50]
-  # end
+  version :medium_avatar do
+    # returns a 50x50 image
+    process :resize_to_fit => [200, 200]
+  end
   
   # def extension_white_list
   #   %w(jpg jpeg gif png)
   # end
+  
   
   def store_dir
     "uploads/locallage"
@@ -22,9 +22,8 @@ class LocallageUploader < CarrierWave::Uploader::Base
   end
 
   protected
-  def secure_token
-    var = :"@#{mounted_as}_secure_token"
-    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
-  end
-  
+    def secure_token
+      var = :"@#{mounted_as}_secure_token"
+      model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
+    end
 end

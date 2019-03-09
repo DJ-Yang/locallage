59ae7db4e271885a8128602afad2b28fe20f66e5
EC2 Default User <ec2-user@ip-172-31-39-27.us-east-2.compute.internal>
2019-03-09 18:05:04 +0000

    fixed resizing image

--- app/uploaders/locallage_uploader.rb
+++ app/uploaders/locallage_uploader.rb
@@ -5,12 +5,23 @@ class LocallageUploader < CarrierWave::Uploader::Base
   
   version :medium do
     # returns a 50x50 image
-    process :resize_to_fit => [200, 200]
+    process :my_resize => [200, 200]
+  end
+  
+  version :personal do
+    process :resize_to_fit => [500, 500]
   end
   
   # def extension_white_list
   #   %w(jpg jpeg gif png)
   # end
+  
+  def my_resize(width, height)
+    manipulate! do |img|
+      img.resize "#{width}x#{height}!"
+      img
+    end
+  end
   
   
   def store_dir

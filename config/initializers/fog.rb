f240b0ce743bb0af9df85ecd4c4f111509bb41fd
EC2 Default User <ec2-user@ip-172-31-28-193.us-east-2.compute.internal>
2019-03-12 12:52:10 +0000

    change account

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -8,7 +8,7 @@ CarrierWave.configure do |config|
     region:                'ap-northeast-2',                  # optional, defaults to 'us-east-1'
     endpoint:              'http://s3.ap-northeast-2.amazonaws.com' # optional, defaults to nil
   }
-  config.fog_directory  = 'cbnu-community'                                      # required
+  config.fog_directory  = 'locallage-image'                                      # required
   config.fog_public     = true                                                 # optional, defaults to true
 #   config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
 end
